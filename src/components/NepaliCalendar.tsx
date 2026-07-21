409b8d1759b7a474795584e1f6a87b15e580f196
Soniya Gurung <soniya.gurung@navyaadvisors.com>
2026-07-21 10:24:30 +0545

    minor issue fixes

--- src/components/NepaliCalendar.tsx
+++ src/components/NepaliCalendar.tsx
@@ -164,7 +164,7 @@ export default function NepaliCalendar({
               },
             },
           }}
-          sx={{minWidth:114, width: 114}}
+          sx={{minWidth:118, width: 118}}
           size="small"
         >
           {formatObj.month.long[type].map((name, index) => (
